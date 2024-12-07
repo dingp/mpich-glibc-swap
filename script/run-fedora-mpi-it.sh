0fcc1371db639a59db6fb8cdb4e175ea804788ab
Pengfei Ding <9438483+dingp@users.noreply.github.com>
2024-12-07 17:10:58 -0600

    Update run-fedora-mpi-it.sh

--- script/run-fedora-mpi-it.sh
+++ script/run-fedora-mpi-it.sh
@@ -2,5 +2,4 @@ podman-hpc run --rm -it -e SLURM_* -e PALS_* -e PMI_* -e LD_LIBRARY_PATH=/opt/ud
 --network=host --pid=host --privileged -v  /etc/podman_hpc/01-mpich.conf:/etc/ld.so.conf.d/02-mpich.conf \
 -v /dev/xpmem:/dev/xpmem -v /dev/shm:/dev/shm -v /dev/cxi0:/dev/cxi0 -v /usr/lib/shifter/mpich-2.2:/opt/udiImage/modules/mpich \
 -v /var/spool/slurmd:/var/spool/slurmd -v /run/munge:/run/munge -v /run/nscd:/run/nscd -v /etc/libibverbs.d:/etc/libibverbs.d \
--v $SCRATCH:/scratch  ghcr.io/dingp/mpich-glibc-swap:latest
-
+-v $SCRATCH:/scratch  ghcr.io/dingp/fedora:26-mpich
